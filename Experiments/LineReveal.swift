ac4bb220a92fd0dc02744fe1caa2f1de4b2fb232
notabhayr <abhayrdesign@gmail.com>
2024-11-16 23:15:05 +0545

    Line reveal on scroll

---
+++ Experiments/LineReveal.swift
@@ -0,0 +1,18 @@
+//
+//  LineReveal.swift
+//  Experiments
+//
+//  Created by Abhay R on 16/11/24.
+//
+
+import SwiftUI
+
+struct LineReveal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineReveal()
+}
